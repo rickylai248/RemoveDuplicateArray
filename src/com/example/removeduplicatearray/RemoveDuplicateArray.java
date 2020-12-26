d52d8ebc06053d24dda0ef51efeced37a540ba05
rickylai <f5x2b@ugrad.cs.ubc.ca>
2020-12-25 20:43:28 -0800

    Setup print statements for current items in list before filtering

--- src/com/example/removeduplicatearray/RemoveDuplicateArray.java
+++ src/com/example/removeduplicatearray/RemoveDuplicateArray.java
@@ -1,7 +1,21 @@
 package com.example.removeduplicatearray;
 
+import java.util.ArrayList;
+
 public class RemoveDuplicateArray {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Initialization Line");
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Angel");
+        names.add("Bob");
+        names.add("Angel");
+        names.add("Carry");
+        names.add("Daniel");
+        names.add("Fiona");
+        names.add("Fiona");
+        System.out.println("Checking for duplicates...");
+        for (String str : names) {
+            System.out.println(str + " ");
+        }
     }
 }
